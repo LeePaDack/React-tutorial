34ccf6233cbaeb80b179f5e4ffc8ddfc7a276084
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-05 14:30:47 +0900

    주석 추가

--- tutorial-one/src/component/R16_Contest4/작성공간.js
+++ tutorial-one/src/component/R16_Contest4/작성공간.js
@@ -9,10 +9,14 @@ const 작성하는공간 = () =>{
 
     const 유저추가 = () => {
         const 유저 = {"inputName" : inputName, "inputPhone" : inputPhone}
-        
+        // 영어는 " " 로 감싸지 않아도 괜찮음
         const 새로운유저 = [...userList, 유저];
         setUserList(새로운유저);
     
+        // 멤버가 저장이 되면 input 에 작성된 내용 모두 지우기
+        setInputName(''); // 이름칸 모두 지우기
+        setInputPhone(''); // 번호칸 모두 지우기
+
     }
 
     return(
