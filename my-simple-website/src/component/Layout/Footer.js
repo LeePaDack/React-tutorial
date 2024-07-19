174dcbb8459fbd346146b1cf30692c784257a0f3
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-19 10:50:52 +0900

    Footer

---
+++ my-simple-website/src/component/Layout/Footer.js
@@ -0,0 +1,17 @@
+const Footer = () => {
+
+    return (
+        <div className="footer">
+            <div className="footer-content">
+                <p>&copy; 2024 LEEGYEJUN portfolio</p>
+                <div className="footer-links">
+                    <a href="https://www.github.com/LeePaDack">깃허브 이동</a>
+                    <a href="https://grizzled-tuck-45d.notion.site/1-299d33a60ce840e6959866fad0f24675?pvs=4">한바탕 노션으로 이동하기</a>
+                </div>
+            </div>
+        </div>
+    );
+
+}
+
+export default Footer;
