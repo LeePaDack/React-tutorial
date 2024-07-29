0133a8dd79945b0bf3590a0e12289be7a14309d6
LeePaDack <151605745+LeePaDack@users.noreply.github.com>
2024-07-29 10:17:17 +0900

    naver login api

--- mysql-front/src/component/UserForm.js
+++ mysql-front/src/component/UserForm.js
@@ -25,6 +25,8 @@ const UserForm = ({addUser}) => {
                     <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required/>
                 </div>
                 <button type="submit">유저 추가하기</button>
+                <br/><br/>
+                <button type="submit">네이버 로그인을 통한 유저 추가하기</button>
             </form>
         </div>
     )
